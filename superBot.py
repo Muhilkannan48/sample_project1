45a758f344b7fd004ce72bd32fa69c0abccea0e2
Muhilkannan48 <muhilkannan48@gmail.com>
2023-08-16 12:13:41 +0530

    Superbot page is modified

--- superBot.py
+++ superBot.py
@@ -6,4 +6,5 @@ diver = webdriver.Chrome()
 
 session = InstaPy(username="testingggggit123", password=password,headless_browser=True)
 session.login()
+
 session.like_by_tags(["bmw", "mercedes"], amount=5)
